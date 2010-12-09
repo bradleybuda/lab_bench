9da2f2368edb032c6280cb8a25bf599117e0132f
Bradley Buda <brad@tracksimple.com>
2010-12-08 16:00:21 -0800

    Don't treat a non-running server as fatal; instead just warn and disable for this test run

--- lib/lab_bench/test_runner.rb
+++ lib/lab_bench/test_runner.rb
@@ -8,34 +8,36 @@ class LabBench::TestRunner < Test::Unit::UI::Console::TestRunner
   def initialize(*args)
     super(*args)
     
-    connect_to_mothership
+    if @lab_bench_enabled = mothership_alive?
+      STDERR.puts "LabBench enabled - go to http://0.0.0.0:9020/ to see test results"
+    else
+      STDERR.puts "WARNING - could not detect LabBench server.  Run lab_bench_server to start it."
+    end
+    
     @guid = "suite_#{rand(2**64).to_s}"
   end
   
   private
 
   def attach_to_mediator
-    # Add listeners for the five main testing events and relay then to the server
-    [
-     Test::Unit::UI::TestRunnerMediator::STARTED,
-     Test::Unit::UI::TestRunnerMediator::FINISHED,
-     Test::Unit::TestResult::FAULT,
-     Test::Unit::TestCase::STARTED,
-     Test::Unit::TestCase::FINISHED,
-    ].each do |event|
-      @mediator.add_listener(event) do |*args|
-        Net::HTTP.post_form(mothership_uri('test_event'), {:event => event, :args => args, :guid => @guid})
+    if @lab_bench_enabled
+      # Add listeners for the five main testing events and relay then to the server
+      [
+       Test::Unit::UI::TestRunnerMediator::STARTED,
+       Test::Unit::UI::TestRunnerMediator::FINISHED,
+       Test::Unit::TestResult::FAULT,
+       Test::Unit::TestCase::STARTED,
+       Test::Unit::TestCase::FINISHED,
+      ].each do |event|
+        @mediator.add_listener(event) do |*args|
+          Net::HTTP.post_form(mothership_uri('test_event'), {:event => event, :args => args, :guid => @guid})
+        end
       end
     end
-
+    
     super
   end
     
-  def connect_to_mothership
-    # TODO probably don't want to abort tests if can't connect, maybe just warn
-    raise 'could not communicate with mothership' unless mothership_alive?
-  end
-  
   def mothership_alive?
     Net::HTTP.get(mothership_uri('ping'))
     true
@@ -43,14 +45,12 @@ class LabBench::TestRunner < Test::Unit::UI::Console::TestRunner
     false
   end
 
-  # TODO memoize
   def mothership_uri(method)
     URI.parse("http://localhost:9020/#{method}")
   end
-  
 end
 
-# Register myself
+# Register myself on load
 require 'test/unit/autorunner'
 Test::Unit::AutoRunner::RUNNERS[:lab_bench] = proc do |r|
   LabBench::TestRunner
